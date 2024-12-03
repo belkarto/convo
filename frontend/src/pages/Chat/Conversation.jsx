706914cd19370c499f6af24ca4fedffbb2791470
brahim elkartouch <belkarto@student.1337.ma>
2024-12-03 15:33:18 +0100

    add conversations list

--- frontend/src/pages/Chat/Conversation.jsx
+++ frontend/src/pages/Chat/Conversation.jsx
@@ -1,5 +1,21 @@
-function Conversation() {
-	return <div>Conversation</div>;
+import propTypes from "prop-types";
+
+function Conversation({ selectedConversation }) {
+	return (
+		<div className="w-1/2">
+			{selectedConversation ? (
+				selectedConversation.name
+			) : (
+				<div className="flex items-center justify-center h-full text-3xl text-gray-500">
+					Select a chat to start messaging
+				</div>
+			)}
+		</div>
+	);
 }
+
+Conversation.propTypes = {
+	selectedConversation: propTypes.object
+};
 
 export default Conversation;
--- frontend/src/pages/Chat/ConverstionsList.jsx
+++ frontend/src/pages/Chat/ConverstionsList.jsx
@@ -1,5 +1,47 @@
-function ConverstionsList() {
-	return <div>ConverstionsList</div>;
+import propTypes from "prop-types";
+
+function ConverstionsList({ conversations, setSelectedConversation }) {
+	return (
+		<div className="w-1/4  h-screen overflow-y-auto">
+			<div className="p-4 border-b flex justify-between items-center">
+				<h2 className="text-xl font-bold">Chats</h2>
+			</div>
+			{conversations.map((chat) => (
+				<div
+					key={chat.id}
+					className={`p-4 flex items-center hover:bg-vibrant-pink cursor-pointer 
+            ${chat.unreadMessages > 0 ? "bg-blue-50" : ""}`}
+					onClick={() => setSelectedConversation(chat)}
+				>
+					<div className="flex-grow">
+						<div className="flex justify-between">
+							<span className="font-semibold">{chat.name}</span>
+							<span className="text-xs text-gray-500">
+								{chat.lastMessageTime}
+							</span>
+						</div>
+						<div className="flex justify-between">
+							<p
+								className={`text-sm truncate ${chat.isItOwnMessage ? "text-gray-500" : "font-semibold"}`}
+							>
+								{chat.lastMessage}
+							</p>
+							{chat.unreadMessages > 0 && (
+								<span className="bg-blue-500 text-white rounded-full px-2 py-0.5 text-xs">
+									{chat.unreadMessages}
+								</span>
+							)}
+						</div>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 }
+
+ConverstionsList.propTypes = {
+	conversations: propTypes.array.isRequired,
+	setSelectedConversation: propTypes.func.isRequired
+};
 
 export default ConverstionsList;
