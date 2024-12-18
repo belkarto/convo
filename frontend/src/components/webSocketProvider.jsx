0bb402c1b837a1a781bac44ed6df44d25f40caa5
Brahim Elkartouch <belkarto@students.1337.ma>
2024-12-18 16:12:35 +0100

    remove socket.io

--- frontend/src/components/webSocketProvider.jsx
+++ frontend/src/components/webSocketProvider.jsx
@@ -1,18 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { io } from "socket.io-client";
+import useAuth from "../hooks/useAuth.js";
 import { SocketContext } from "../constants";
 
 const WebSocketProvider = () => {
 	const [socket, setSocket] = useState(null);
 	const { Provider } = SocketContext;
+	const { user } = useAuth();
 	useEffect(() => {
-		const sock = io("ws://localhost:8000/ws/chat");
+		const sock = new WebSocket(`ws://localhost:8000/?au=${user?.access}`);
 
 		setSocket(sock);
 
 		return () => {
-			sock.disconnect();
+			sock.close();
 		};
 	}, []);
 	return (
