b66db50f5eef164d59bc485e52940e3fb4f3fe09
brahim elkartouch <belkarto@student.1337.ma>
2024-12-23 05:55:20 +0100

    remove auth from chat app

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -5,8 +5,5 @@ from chat.views import testView
 from .auth_views import SignUpView, LoginView, CookieTokenRefreshView
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
-    path("login/", LoginView.as_view(), name="get_token"),
-    path("refresh/", CookieTokenRefreshView.as_view(), name="refresh_token"),
     path("test/", testView.as_view(), name="test_url"),
 ]
